90556735b08889810f91e57297f4f0f346d02beb
PJC-1 <jumpzine@gmail.com>
2017-08-22 21:27:34 -0700

    added a beforeEach hook to drop the collection db

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Going through The Net Ninja's MongoDB Tutorial again.
+
+
+DeprecationWarning: Mongoose: mpromise (mongoose's default promise library) is deprecated, plug in your own promise library instead: http://mongoosejs.com/docs/promises.html
+
+From the connection.js file add this code:
+mongoose.Promise = global.Promise;
+
+We are using the ES6 default promise from the global object and overwriting the promise from mongoose.
--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
-// connect to MongoDB
-mongoose.connect('mongodb://localhost/mongo2');
+// ES6 promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection.once('open', function(){
-    console.log('Connection has been made');
-}).on('error', function(error){
-    console.log('Connection error. ', error);
+// Connect to the db before the tests run
+before(function(done){
+    // connect to MongoDB
+    mongoose.connect('mongodb://localhost/mongo2');
+
+    mongoose.connection.once('open', function(){
+      console.log('Connection has been made');
+      done();
+    }).on('error', function(error){
+      console.log('Connection error. ', error);
+    });
+});
+
+// Drop the characters collection before each test
+beforeEach(function(done){
+    // Drop the collection
+    mongoose.connection.collections.mariochars.drop(function(){
+        done();
+    });
 });
