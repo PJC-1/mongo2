5b704246cce56e36e006b9e7568773e8d437d348
PJC-1 <jumpzine@gmail.com>
2017-08-23 21:50:28 -0700

    added a test updating an author's books SUCCESS

--- test/nesting_test.js
+++ test/nesting_test.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const Author = require('../models/author');
 
 // Describe out tests
-describe('Nesting records', function(){
+describe('Nesting records', function(done){
+
+    beforeEach(function(done){
+        mongoose.connection.collections.authors.drop(function(){
+            done();
+        });
+    });
+
     // Create tests
     it('Creates an author with sub-documents', function(done){
         var pat = new Author({
@@ -18,6 +25,26 @@ describe('Nesting records', function(){
           });
         });
 
+    });
+
+    it('Add a book to an author', function(done){
+        var pat = new Author({
+            name: 'Patric Rothfuss',
+            books: [{title:'Name of the Wind', pages: 400}]
+        });
+
+        pat.save().then(function(){
+            Author.findOne({name:'Patric Rothfuss'}).then(function(record){
+                // add a book to the books array
+                record.books.push({title:"Wise Man's Fear", pages: 500});
+                record.save().then(function(){
+                    Author.findOne({name:'Patric Rothfuss'}).then(function(record){
+                        assert(record.books.length === 2);
+                        done();
+                    });
+                });
+            });
+        });
     });
 
 });
